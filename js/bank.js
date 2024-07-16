f0c3326d6d1e9b2330b22bf56c11ecaa205a4598
ranamiah221 <ranarasul21@gmail.com>
2024-07-16 11:35:00 +0600

    added bank.js and added funtionality of bank deposit section js part

---
+++ js/bank.js
@@ -0,0 +1,21 @@
+document.getElementById('deposit-btn').addEventListener('click',function(){
+    const newDepositAmount=document.getElementById('new-deposit')
+    const newDepositString=newDepositAmount.value;
+    const newDeposit= parseFloat(newDepositString);
+    const previousDepositAmount=document.getElementById('previous-deposit');
+    const previousDepositString=previousDepositAmount.innerText;
+    const previousDeposit=parseFloat(previousDepositString);
+    const updatedDeposit = previousDeposit + newDeposit;
+    previousDepositAmount.innerText=updatedDeposit;
+
+    const totalDepositFlied=document.getElementById('total-deposit');
+    const totalDepositString=totalDepositFlied.innerText;
+    const totalDeposit=parseFloat(totalDepositString);
+    const currentTotalDeposit=totalDeposit + newDeposit;
+    totalDepositFlied.innerText=currentTotalDeposit;
+
+    // clear the flied
+    newDepositAmount.value='';
+    
+
+})
